65342056125edfe57a1b2be8d3e553edadce8f64
mokyoungg <mokyoungg@gmail.com>
2024-06-21 18:18:18 +0900

    feat: base-blog-em, fetch comments using queryKey array

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,9 +1,25 @@
 import { fetchComments } from "./api";
 import "./PostDetail.css";
+import { useQuery } from "@tanstack/react-query";
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong.</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
