9c5dd168c460cf444fe1f2fe1ac393d63f8e0cc3
mokyoungg <mokyoungg@gmail.com>
2024-06-21 16:57:59 +0900

    feat: base-blog-em, create query with useQuery

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -8,8 +9,14 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (!data) {
+    return <div />;
+  }
 
   return (
     <>
