184e30aa6a29cb820637d23a47f277bf490c4c53
mokyoungg <mokyoungg@gmail.com>
2024-06-27 17:15:23 +0900

    feat: base-lazy-days, test useStaff custom hook

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,9 +1,20 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { useStaff } from "../hooks/useStaff";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
 
 test("filter staff", async () => {
-  // the magic happens here
+  const { result } = renderHook(() => useStaff(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for the staff to populate
+  await waitFor(() => expect(result.current.staff).toHaveLength(4));
+
+  // set to filter for only staff who give massage
+  act(() => result.current.setFilter("facial"));
+
+  // wait for the staff list to display only 3
+  await waitFor(() => expect(result.current.staff).toHaveLength(3));
 });
